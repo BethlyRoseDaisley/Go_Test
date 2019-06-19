66ff7f600214d47ffee8ce4bc159b5bd937523bc
sola <ailumiyana@sola.com>
2019-06-19 19:49:15 +0800

    sync

--- time.go
+++ time.go
@@ -19,23 +19,26 @@ func main(){
 
 
 
-	go func()  {
-		tick := time.Tick(1000 * time.Millisecond)
-		boom := time.After(5000 * time.Millisecond)
+	go func() {
+		tick1 := time.Tick(1000 * time.Millisecond)
+		tick2 := time.After(5000 * time.Millisecond)
+		cnt := 0
 		for {
 			select {
-			case <-tick:
-				fmt.Println("tick.")
-			case <-boom:
-				fmt.Println("BOOM!")
-			default:
-				fmt.Println(" .")
-				time.Sleep(50 * time.Millisecond)
+			case <-tick1:
+				fmt.Println("tick happen / 1s")
+				cnt = cnt + 1
+				fmt.Println("cnt : ", cnt)
+				if cnt > 10 {
+					return
+				}
+			case <-tick2:
+				fmt.Println("tick happen once 5s")
 			}
 		}
 	}()
 
-	for {
+	for {//必须 x time.Second 默认单位是ns， 习惯性写1，会让cpu%100 哈。
 		time.Sleep(1*time.Second)
 	}
 }
